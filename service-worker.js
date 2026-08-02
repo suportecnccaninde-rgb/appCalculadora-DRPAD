097f9e7a94040dc317928261b502e7a94f7de875
suportecnccaninde-rgb <suportecnccaninde@gmail.com>
2026-08-02 14:51:21 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "drpad-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icone.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
